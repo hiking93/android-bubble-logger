1a40cb1ec52722b78e0db9a70f188002ccbec01c
Hiking <tom_xj_cheng@hotmail.com>
2020-06-25 20:39:56 +0800

    style: Use new system window APIs.

--- example/src/main/java/com/hiking/bubblelogger/example/extension/ActivityExtensions.kt
+++ example/src/main/java/com/hiking/bubblelogger/example/extension/ActivityExtensions.kt
@@ -1,12 +1,18 @@
 package com.hiking.bubblelogger.example.extension
 
 import android.app.Activity
+import android.os.Build
 import android.view.View
 
 fun Activity.applyEdgeToEdge() {
-    window.decorView.apply {
-        systemUiVisibility = systemUiVisibility or
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        window.setDecorFitsSystemWindows(true)
+    } else {
+        window.decorView.apply {
+            @Suppress("DEPRECATION")
+            systemUiVisibility = systemUiVisibility or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        }
     }
 }
